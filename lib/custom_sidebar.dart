dfb7f64088d50741ebdad5c5b9ca04016ab3711f
abarrionuevo <abarrionuevo@encodesa.com.ar>
2024-12-19 11:01:27 -0300

    added customizable icon button and expansion is now optional

--- lib/custom_sidebar.dart
+++ lib/custom_sidebar.dart
@@ -13,6 +13,8 @@ class CustomSidebar extends StatefulWidget {
   final Color? drawerBackgroundColor;
   final Color? drawerIconColor;
   final Icon? drawerIcon;
+  final bool expandable;
+  final Widget? expansionButton;
 
   const CustomSidebar({
     super.key,
@@ -25,9 +27,11 @@ class CustomSidebar extends StatefulWidget {
     this.drawerBackgroundColor,
     this.drawerIconColor,
     this.drawerIcon,
+    this.expansionButton,
     required this.scaffoldKey,
     required this.child,
     required this.sideBarItems,
+    required this.expandable,
   });
 
   @override
@@ -39,50 +43,70 @@ class _CustomSidebarState extends State<CustomSidebar> {
 
   @override
   Widget build(BuildContext context) {
-    final double expandedWidth = widget.width ?? MediaQuery
-        .of(context)
-        .size
-        .width * 0.2;
+    final double expandedWidth =
+        widget.width ?? MediaQuery.of(context).size.width * 0.2;
 
-    final responsive = MediaQuery
-        .sizeOf(context)
-        .width > widget.changeToResponsiveWidth;
+    final responsive =
+        MediaQuery.sizeOf(context).width > widget.changeToResponsiveWidth;
 
     return Scaffold(
       key: widget.scaffoldKey,
       drawer: mobileSidebar(),
-      body: responsive ? Row(
+      body: responsive
+          ? Row(
         children: <Widget>[
-          AnimatedContainer(
+          widget.expandable
+              ? AnimatedContainer(
             duration: const Duration(milliseconds: 300),
-            width: _isExpanded ? expandedWidth : widget.collapsedWidth ?? 60,
+            width: _isExpanded
+                ? expandedWidth
+                : widget.collapsedWidth ?? 60,
             color: widget.sidebarBackgroundColor ?? Colors.blue,
             child: Column(
               children: [
                 expansionButton(),
-                if (_isExpanded) widget.header ?? const SizedBox.shrink(),
+                if (_isExpanded)
+                  widget.header ?? const SizedBox.shrink(),
                 Expanded(
                   child: ListView(
-                    padding: const EdgeInsets.symmetric(horizontal: 3),
+                    padding:
+                    const EdgeInsets.symmetric(horizontal: 3),
                     children: widget.sideBarItems,
                   ),
                 ),
-                if (_isExpanded) widget.footer ?? const SizedBox.shrink(),
+                if (_isExpanded)
+                  widget.footer ?? const SizedBox.shrink(),
+              ],
+            ),
+          )
+              : Container(
+            width: expandedWidth,
+            color: widget.sidebarBackgroundColor ?? Colors.blue,
+            child: Column(
+              children: [
+                widget.header ?? const SizedBox.shrink(),
+                Expanded(
+                  child: ListView(
+                    padding:
+                    const EdgeInsets.symmetric(horizontal: 3),
+                    children: widget.sideBarItems,
+                  ),
+                ),
+                widget.footer ?? const SizedBox.shrink(),
               ],
             ),
           ),
           Expanded(child: widget.child),
         ],
-      ) : mobileView(),
+      )
+          : mobileView(),
     );
   }
 
   Drawer mobileSidebar() {
     return Drawer(
         backgroundColor: widget.drawerBackgroundColor,
-        width: MediaQuery
-            .sizeOf(context)
-            .width * 0.8,
+        width: MediaQuery.sizeOf(context).width * 0.8,
         child: Column(
           children: [
             widget.header ?? const SizedBox.shrink(),
@@ -94,8 +118,7 @@ class _CustomSidebarState extends State<CustomSidebar> {
             ),
             widget.footer ?? const SizedBox.shrink(),
           ],
-        )
-    );
+        ));
   }
 
   Align buildDrawerButton() {
@@ -106,8 +129,9 @@ class _CustomSidebarState extends State<CustomSidebar> {
         child: IconButton(
           color: widget.drawerIconColor ?? Colors.black,
           iconSize: 45,
-          onPressed: ()=> 
-            widget.scaffoldKey.currentState?.openDrawer(),
+          onPressed: () {
+            widget.scaffoldKey.currentState?.openDrawer();
+          },
           icon: widget.drawerIcon ?? const Icon(Icons.menu),
         ),
       ),
@@ -116,18 +140,15 @@ class _CustomSidebarState extends State<CustomSidebar> {
 
   Align expansionButton() {
     return Align(
-      alignment: _isExpanded ?
-      Alignment.topRight :
-      Alignment.center,
-      child: IconButton(
-        icon: Icon(
-          size: 15,
-          _isExpanded ?
-          Icons.arrow_back_ios :
-          Icons.arrow_forward_ios,
-          color: Colors.white,
-        ),
-        onPressed: () {
+      alignment: _isExpanded ? Alignment.topRight : Alignment.center,
+      child: InkWell(
+        child: widget.expansionButton ??
+            Icon(
+              size: 15,
+              _isExpanded ? Icons.arrow_back_ios : Icons.arrow_forward_ios,
+              color: Colors.white,
+            ),
+        onTap: () {
           setState(() {
             _isExpanded = !_isExpanded;
           });
@@ -137,9 +158,11 @@ class _CustomSidebarState extends State<CustomSidebar> {
   }
 
   Column mobileView() {
-    return Column(children: [
-      buildDrawerButton(),
-      Expanded(child: widget.child),
-    ],);
+    return Column(
+      children: [
+        buildDrawerButton(),
+        Expanded(child: widget.child),
+      ],
+    );
   }
 }
